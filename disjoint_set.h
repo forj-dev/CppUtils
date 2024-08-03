2b06f9ddd745f683cafa8df1a20756d5090b0f90
forj-dev <not-useful-at-all@hotmail.com>
2024-08-03 15:58:57 +0800

    disjoint_set.h
    fix: add "public:"

--- disjoint_set.h
+++ disjoint_set.h
@@ -1,18 +1,25 @@
+
 #ifndef _Disjoint_Set_
 #define _Disjoint_Set_
+
 #include<bits/stdc++.h>
+
 class Disjoint_Set{
+private:
 	std::vector<size_t> fa;
+public:
 	Disjoint_Set(size_t size){
 		fa=std::vector<size_t>(size);
 		for(size_t i=0;i<size;i++)
 			fa[i]=i;
 	}
+	
 	size_t getFather(size_t node){
 		if(fa[node]==node)
 			return node;
 		return fa[node]=getFather(fa[node]);
 	}
+	
 	bool merge(size_t a,size_t b){
 		size_t f_a=getFather(a);
 		size_t f_b=getFather(b);
@@ -22,4 +29,6 @@ class Disjoint_Set{
 		return true;
 	}
 };
+
 #endif
+
